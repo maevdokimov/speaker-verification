c66806313dd3fb4704437813fbe3565b2d5d95d4
maevdokimov <maximevdfox@gmail.com>
2022-05-18 18:31:55 +0000

    feat: :sparkles: LDE pooling layer

--- src/modules/pooling_layers.py
+++ src/modules/pooling_layers.py
@@ -70,3 +70,67 @@ class AttentivePoolingLayer(NeuralModule):
             logits = F.linear(pre_norm_emb, self.out_weight, self.out_bias)
 
         return logits, pre_norm_emb
+
+
+class LDEPoolingLayer(NeuralModule):
+    def __init__(self, input_dim: int, num_clusters: int, num_classes: int, angular: bool):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.num_clusters = num_clusters
+        self.angular = angular
+        self.num_classes = num_classes
+
+        self.dictionary_components = nn.Parameter(torch.empty(num_clusters, input_dim))
+        nn.init.uniform_(self.dictionary_components, -1, 1)
+
+        self.out_weight = nn.Parameter(torch.empty(num_classes, input_dim))
+        nn.init.kaiming_uniform_(self.out_weight, a=math.sqrt(5))
+        if not angular:
+            self.out_bias = nn.Parameter(torch.empty(num_classes))
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.out_weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            nn.init.uniform_(self.out_bias, -bound, bound)
+
+        self.embed_proj = nn.Linear(input_dim * num_clusters, input_dim)
+
+    @property
+    def input_types(self):
+        return {
+            "encoder_outputs": NeuralType(("B", "D", "T"), AcousticEncodedRepresentation()),
+            "encoder_lengths": NeuralType(tuple("B"), LengthsType()),
+        }
+
+    @property
+    def output_types(self):
+        return {
+            "logits": NeuralType(("B", "D"), LogitsType()),
+            "embs": NeuralType(("B", "D"), AcousticEncodedRepresentation()),
+        }
+
+    @typecheck()
+    def forward(self, encoder_outputs, encoder_lengths):
+        outputs_transposed = encoder_outputs.transpose(1, 2)  # B x T x D
+
+        r = outputs_transposed.unsqueeze(2) - self.dictionary_components  # B x T x n_clust x D
+        r_norm = F.normalize(r, p=2, dim=-1)  # B x T x n_clust
+        w = F.softmax(-r_norm, dim=-1).unsqueeze(1)  # B x T x n_clust x 1
+
+        mask = ~get_mask_from_lengths(encoder_lengths, max_len=encoder_outputs.shape[2])
+        mask = mask.view(*mask.shape, 1, 1)
+        masked_w = torch.masked_fill(w, mask, 0.0)  # B x T x n_clust x 1
+
+        masked_w = masked_w / (torch.sum(masked_w, dim=1, keepdim=True) + 1e-9)  # B x T x n_clust
+        embeddings = torch.sum(masked_w * r, dim=1)  # B x n_clust x D
+        embeddings = torch.reshape(embeddings.shape[0], -1)  # B x n_clust * D
+        embeddings = self.embed_proj(embeddings)  # B x D
+
+        if self.angular:
+            normalized_emb = F.normalize(embeddings, p=2, dim=1)
+            normalized_weight = F.normalize(self.out_weight, p=2, dim=1)
+
+            logits = F.linear(normalized_emb, normalized_weight)
+        else:
+            logits = F.linear(embeddings, self.out_weight, self.out_bias)
+
+        return logits, embeddings
